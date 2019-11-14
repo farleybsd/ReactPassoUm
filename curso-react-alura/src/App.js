98022daeae51eff57b138d53d7e2485de38496d1
farleybsd <farley.t.i@hotmail.com>
2019-11-14 14:00:31 -0300

    Primeiro componente

---
+++ curso-react-alura/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Tabela from './tabela'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Tabela/>>
+
+    </div>
+  );
+}
+
+export default App;
